70ec5a17fc57550978342feaa6a67e8b2fe29f55
Blake Embrey <hello@blakeembrey.com>
2015-11-09 15:32:33 -0800

    Test typing installation alongside test suite

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
 var Ajv = require('ajv')
 var glob = require('glob')
 var parse = require('parse-json')
-var readFileSync = require('fs').readFileSync
+var readFile = require('fs').readFile
 var join = require('path').join
+var Batch = require('batch')
+var typings = require('typings')
+var arrify = require('arrify')
 var schema = require('./schema.json')
 
 var ajv = new Ajv()
+var filesBatch = new Batch()
+var typingsBatch = new Batch()
 var validate = ajv.compile(schema)
+
+filesBatch.concurrency(10)
+typingsBatch.concurrency(5)
 
 glob('{ambient,bower,common,github,npm}/**/*.json', function (err, files) {
   if (err) {
@@ -14,17 +22,66 @@ glob('{ambient,bower,common,github,npm}/**/*.json', function (err, files) {
     process.exit(1)
   }
 
-  files.forEach(function (filename) {
-    var data = parse(readFileSync(join(__dirname, filename), 'utf8'))
+  files.forEach(function (file) {
+    filesBatch.push(function (done) {
+      readFile(join(__dirname, file), 'utf8', function (err, contents) {
+        var data
 
-    var valid = validate(data)
+        if (err) {
+          return done(err)
+        }
 
-    if (!valid) {
-      console.error('Invalid JSON for "' + filename + '":')
-      console.error(ajv.errorsText(validate.errors))
-      process.exit(1)
-    }
+        try {
+          data = parse(contents)
+        } catch (err) {
+          return done(err)
+        }
+
+        var valid = validate(data)
+
+        if (!valid) {
+          return done(new Error('Invalid JSON for "' + file + '":\n' + ajv.errorsText(validate.errors)))
+        }
+
+        // Push all typings installation tests into a batch executor.
+        Object.keys(data.version).forEach(function (version) {
+          arrify(data.version[version]).forEach(function (location) {
+            typingsBatch.push(function (done) {
+              typings.installDependency(location, { cwd: __dirname, name: 'test' })
+                .then(function () {
+                  return done()
+                }, done)
+            })
+          })
+        })
+
+        return done()
+      })
+    })
   })
 
-  console.log('The registry is valid, thanks to you!')
+  filesBatch.on('progress', function (e) {
+    console.log('Reading files: ' + e.percent + '%')
+  })
+
+  typingsBatch.on('progress', function (e) {
+    console.log('Typings installation: ' + e.percent + '%')
+  })
+
+  filesBatch.end(function (err) {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+
+    typingsBatch.end(function (err) {
+      if (err) {
+        console.log(err)
+        process.exit(1)
+      }
+
+      console.log('The registry is valid, thank you!')
+      process.exit(0)
+    })
+  })
 })
